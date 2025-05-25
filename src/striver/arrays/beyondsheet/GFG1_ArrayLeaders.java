606c242155f3a73270f1133dce35e58042b10000
Piyush Bhatta <piyushbhatta65@gmail.com>
2025-05-25 22:43:19 +0530

    Added Java Questions

---
+++ src/striver/arrays/beyondsheet/GFG1_ArrayLeaders.java
@@ -0,0 +1,25 @@
+package striver.arrays.beyondsheet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GFG1_ArrayLeaders {
+    public static void main(String[] args) {
+        int[] arr={16, 17, 4, 3, 5, 2};
+        System.out.println(leaders(arr));
+    }
+    public static ArrayList<Integer> leaders(int[] arr){
+        int n=arr.length-1;
+        int maxSoFar=n;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[maxSoFar]);
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>=arr[maxSoFar]){
+                maxSoFar=i;
+                list.add(arr[maxSoFar]);
+            }
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        return list;
+    }
+}
