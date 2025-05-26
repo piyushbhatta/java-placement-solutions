b3a8703a664d1e76047eaf933e9def4f6807060e
Piyush Bhatta <piyushbhatta65@gmail.com>
2025-05-26 20:36:09 +0530

    Added Java Questions

---
+++ src/BinarySearch/GFG_Implement_UpperBound.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class GFG_Implement_UpperBound {
+    public static void main(String[] args) {
+
+    }
+    public static int upperBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;  // First index where arr[low] > target
+    }
+}
