a1ab3e4554f2f7afa39e07982fa12e85d05856ac
Piyush Bhatt <piyushbhatta65@gmail.com>
2025-06-26 23:32:13 +0530

    Java Problem Solution

---
+++ src/Learning_LinkedList_II/leetcode138_copyRandomList.java
@@ -0,0 +1,41 @@
+package Learning_LinkedList_II;
+
+import java.util.HashMap;
+
+public class leetcode138_copyRandomList {
+    public static class Node{
+        int val;
+        Node next;
+        Node random;
+        Node(){
+
+        }
+        Node(int val){
+            this.val=val;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+    public static Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            Node copy = map.get(curr);
+            copy.next = map.get(curr.next);
+            copy.random = map.get(curr.random);
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+}
